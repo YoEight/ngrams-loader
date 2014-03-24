b9e00bac23259fa2c4386cbd280685535c50421d
YoEight <yo.eight@gmail.com>
2014-03-24 23:20:20 +0100

    Implement Main module

--- Main.hs
+++ Main.hs
@@ -9,8 +9,95 @@
 -- Portability : non-portable
 --
 ----------------------------------------------------------------------------
-import Data.Ngrams
-----------------------------------------------------------------------------
+module Main where
 
+----------------------------------------------------------------------------
+import Data.List (find)
+
+----------------------------------------------------------------------------
+import System.Console.ParseArgs
+
+----------------------------------------------------------------------------
+import Data.Ngrams
+
+----------------------------------------------------------------------------
+-- | Command-line Option type
+data Option = Bi
+            | Tri
+            | Quad
+            | Penta
+            | Create
+            | File
+            | DB deriving (Ord, Eq, Show, Enum)
+
+----------------------------------------------------------------------------
 main :: IO ()
-main = print "Hello World"
+main = do
+    as <- parseArgsIO ArgsComplete desc
+    let nType  = parserType as
+        file   = getRequiredArg as File
+        db     = getRequiredArg as DB
+        create = gotArg as Create
+        mk     = mkProcess file db create
+
+        ngram Bi    = mk parserBigram bigramCommand
+        ngram Tri   = mk parserTrigram trigramCommand
+        ngram Quad  = mk parserQuadgram quadgramCommand
+        ngram Penta = mk parserPentagram pentagramCommand
+        ngram _     = error "The impossible happens"
+
+    case nType of
+        Nothing -> print "Please toggled one of N-grams type"
+        Just p  -> runProcess_ $ ngram p
+
+----------------------------------------------------------------------------
+-- | Command-line Options parser description
+desc :: [Arg Option]
+desc = [ Arg { argIndex = Bi
+             , argAbbr  = Just '2'
+             , argName  = Just "bigram"
+             , argData  = Nothing
+             , argDesc  = "Parses bigrams"
+             }
+       , Arg { argIndex = Tri
+             , argAbbr  = Just '3'
+             , argName  = Just "trigram"
+             , argData  = Nothing
+             , argDesc  = "Parses trigrams"
+             }
+       , Arg { argIndex = Quad
+             , argAbbr  = Just '4'
+             , argName  = Just "quadgram"
+             , argData  = Nothing
+             , argDesc  = "Parses 4-grams"
+             }
+       , Arg { argIndex = Penta
+             , argAbbr  = Just '5'
+             , argName  = Just "pentagram"
+             , argData  = Nothing
+             , argDesc  = "Parses 5-grams"
+             }
+       , Arg { argIndex = Create
+             , argAbbr  = Just 'c'
+             , argName  = Just "create"
+             , argData  = Nothing
+             , argDesc  = "Creates table before inserts"
+             }
+       , Arg { argIndex = File
+             , argAbbr  = Nothing
+             , argName  = Nothing
+             , argData  = argDataRequired "n-grams file" ArgtypeString
+             , argDesc  = "N-grams file"
+             }
+       , Arg { argIndex = DB
+             , argAbbr  = Nothing
+             , argName  = Nothing
+             , argData  = argDataRequired "SQLite file" ArgtypeString
+             , argDesc  = "SQlite db file"
+             }
+       ]
+
+----------------------------------------------------------------------------
+-- | Gets toggled Ngrams flag
+parserType :: Args Option -> Maybe Option
+parserType as = find (gotArg as) [Bi .. Penta]
--- lib/Data/Ngrams.hs
+++ lib/Data/Ngrams.hs
@@ -13,9 +13,31 @@ module Data.Ngrams
     ( module Data.Ngrams.Database.Sqlite
     , module Data.Ngrams.Parser
     , module Data.Ngrams.Process
+    , mkProcess
+    , runProcess_
     ) where
+
+----------------------------------------------------------------------------
+import Control.Monad.Trans.Resource (ResourceT, runResourceT)
+import Data.Attoparsec.Text         (Parser)
+import Data.Machine                 (ProcessT, runT_, (~>))
+import Data.Text                    (Text)
 
 ----------------------------------------------------------------------------
 import Data.Ngrams.Database.Sqlite
 import Data.Ngrams.Parser
 import Data.Ngrams.Process
+
+----------------------------------------------------------------------------
+mkProcess :: FilePath
+          -> FilePath
+          -> Bool
+          -> Parser a
+          -> Command a
+          -> ProcessT (ResourceT IO) Text ()
+mkProcess file db create parser cmd =
+    sourceLines file ~> parseLine parser ~> saveDB create db cmd
+
+----------------------------------------------------------------------------
+runProcess_ :: ProcessT (ResourceT IO) Text a -> IO ()
+runProcess_ = runResourceT . runT_
